e8255ee49ae012bf96baa55f9922542169a65d1d
tomjames156 <tomjames156@gmail.com>
2026-04-13 23:53:35 +0100

    updated english strings

--- js/i18n-config.js
+++ js/i18n-config.js
@@ -11,23 +11,62 @@ i18next
     }
   }, function(err, t) {
     if (err) return console.error('Something went wrong loading i18n:', err);
-    
-    console.log('i18next is ready!');
-    console.log('Current Language:', i18next.language);
-    console.log('Testing a key:', i18next.t('ui.title'));
     // Once initialized, update the initial UI
     updateContent();
   });
+
+function updateScoreUI(score){
+  const lng = i18next.language;
+
+  const formattedNumber = new Intl.NumberFormat(lng).format(score);
+
+  const scoreHeading = document.querySelector('[data-i18n="ui.score_count"]');
+  if (scoreHeading) {
+    scoreHeading.innerHTML = i18next.t('ui.score_count', { val: formattedNumber });
+  }
+}
+
+function updateHiScoreUI(hiscore){
+  const lng = i18next.language;
+
+  const formattedNumber = new Intl.NumberFormat(lng).format(hiscore);
+
+  const scoreHeading = document.querySelector('[data-i18n="ui.highscore"]');
+  if (scoreHeading) {
+    scoreHeading.innerHTML = i18next.t('ui.highscore', { val: formattedNumber });
+  }
+}
+
+function updateFinalScoreUI(score){
+  const lng = i18next.language;
+
+  const formattedNumber = new Intl.NumberFormat(lng).format(score);
+
+  const scoreHeading = document.querySelector('[data-i18n="ui.score"]');
+  if (scoreHeading) {
+    scoreHeading.innerHTML = i18next.t('ui.score', { val: formattedNumber });
+  }
+}
 
 function updateContent() {
     // document.querySelector('[data-i18n="ui.title"]').innerHTML = i18next.t('ui.title'); testing purposes
   // Find all elements with the 'data-i18n' attribute
   const elements = document.querySelectorAll('[data-i18n]');
+
+  currentScore = parseInt(document.getElementById('score').textContent.replace(/,/g, '')) || 0;
+  currentHighScore = parseInt(document.getElementById('hiscore').textContent.replace(/,/g, '')) || 0;
   
   elements.forEach(el => {
     const key = el.getAttribute('data-i18n');
-    el.innerHTML = i18next.t(key);
+
+    if(key !== 'ui.score' && key !== 'ui.highscore' && key !== 'ui.score_count') {
+      el.innerHTML = i18next.t(key);
+    }
   });
+
+  updateScoreUI(currentScore);
+  updateHiScoreUI(currentHighScore);
+  updateFinalScoreUI(currentScore);
 
   // Handle RTL layout if language is Arabic
   const currentLang = i18next.language;
